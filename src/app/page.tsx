18c2593ae365ec59c0d31225ad4ac8d223a0b99d
PauliusTaraskevicius <fixas1029@gmail.com>
2025-06-09 16:11:46 +0300

    session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <div className=""></div>;
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  return <div>Only visible to authorized users<Button onClick={() => mutate()}>Logout</Button></div>;
 }
