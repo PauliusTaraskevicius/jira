5d8dabd07fce4f5837915ce08e80c3230f8fd7c8
PauliusTaraskevicius <fixas1029@gmail.com>
2025-06-17 13:38:02 +0300

    refactoring server queries

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,4 @@
-import { getCurrent } from "@/features/auth/actions";
+import { getCurrent } from "@/features/auth/queries";
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
 import { redirect } from "next/navigation";
 
@@ -6,7 +6,7 @@ const SignUpPage = async () => {
   const user = await getCurrent();
 
   if (user) redirect("/");
-  
+
   return <SignUpCard />;
 };
 
